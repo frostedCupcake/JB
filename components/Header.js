4132a40f4a2a67443332d45ed409fac461ea6a41
jaswanth <jaswanthbeere@gmail.com>
2023-04-24 01:02:59 +0530

    made about and contact page responsive

--- components/Header.js
+++ components/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = () => {
   return (
     <div
-      className="flex flex-col justify-center fixed top-0 left-0
+      className="hidden lg:flex flex-col justify-center fixed top-0 left-0
      items-center  bg-white/70 shadow-2xl h-[90vh] w-[30%] p-[2rem] m-[2rem] rounded-md"
     ></div>
   );
--- components/Projects.js
+++ components/Projects.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Projects = () => {
   return (
-    <div className="bg-white/70 h-[90vh] rounded-md shadow-2xl m-[2rem] p-[2rem] w-[60%]">
+    <div className="bg-white/70 h-[90vh] rounded-md shadow-2xl m-[2rem] p-[2rem] w-[90%] lg:w-[60%]">
       <div className="">
         <h1 className="text-[5rem] my-5">My Works.</h1>
         <div className="text-black tracking-wider flex flex-col justify-center items-center leading-10"></div>
