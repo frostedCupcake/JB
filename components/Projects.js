b67777b7508a039bfa23b00dea41e2e98b6fb791
jaswanth <jaswanthbeere@gmail.com>
2023-04-24 11:07:00 +0530

    created my works. template

--- components/Projects.js
+++ components/Projects.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import ProjectCard from './ProjectCard';
 
 const Projects = () => {
   return (
-    <div className="bg-white/70 h-[90vh] rounded-md shadow-2xl m-[2rem] p-[2rem] w-[90%] lg:w-[60%]">
+    <div className="bg-white/70 md:h-[90vh] rounded-md shadow-2xl mx-auto lg:mx-[2rem] my-[2rem] py-[2rem] px-[1rem] md:p-[2rem] w-[90%] lg:w-[60%] text-black h-fit">
       <div className="">
-        <h1 className="text-[5rem] my-5">My Works.</h1>
-        <div className="text-black tracking-wider flex flex-col justify-center items-center leading-10"></div>
+        <h1 className="min-[300px]:text-[3.5rem] text-[4rem] md:text-[4.5rem] lg:text-[4.5rem]  xl:text-[5rem] my-2 lg:my-5 text-center md:text-left">
+          My Works.
+        </h1>
+        <div className="text-black tracking-wider flex flex-col justify-center items-center leading-10 mx-auto">
+          <div className="w-full grid grid-cols-3 grid-rows-2 gap-2 mx-auto">
+            <ProjectCard />
+            <ProjectCard />
+            <ProjectCard />
+            <ProjectCard />
+            <ProjectCard />
+            <ProjectCard />
+          </div>
+        </div>
       </div>
     </div>
   );
