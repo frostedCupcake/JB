46b53496b881197eefb949b3a33ea3c10f4bf43f
jaswanth <jaswanthbeere@gmail.com>
2023-04-27 17:48:27 +0530

    changed text, jaswanth -> I

--- components/About.js
+++ components/About.js
@@ -9,16 +9,16 @@ const About = () => {
         </h1>
         <div className="text-black tracking-wider flex flex-col justify-center items-center gap-5 min-[300px]:leading-7 md:leading-10 xl:leading-10 min-[1700px]:leading-[3rem]">
           <p className="min-[300px]:text-[1.1rem] text-[1.2rem] md:text-[1.4rem] lg:text-[1.4rem] xl:text-[1.5rem] min-[1700px]:text-[1.6rem]">
-            Jaswanth was born in India. He’s currently a sophomore student
-            studying at the Indian Institute of Technology Hyderabad.
+            I was born in India. I’m currently a sophomore student studying at
+            the Indian Institute of Technology Hyderabad.
           </p>
           <p className="min-[300px]:text-[1.1rem] text-[1.2rem] md:text-[1.4rem] lg:text-[1.4rem] xl:text-[1.5rem] 2xl:text-[1.6rem]">
-            Jaswanth enjoys exploring his creative side by indulging in
-            gardening, freelancing, software development & design. He also loves
-            to read books and expand his knowledge, seeking inspiration from a
-            wide range of sources. Some of his favorite books include- Rework:
-            Change the Way You Work Forever, Gene Machine: The Race to Decipher
-            the Secrets of the Ribosome.
+            I enjoy exploring my creative side by indulging in gardening,
+            freelancing, software development & design. I also love to read
+            books and expand my knowledge, seeking inspiration from a wide range
+            of sources. Some of my favorite books include- Rework: Change the
+            Way You Work Forever, Gene Machine: The Race to Decipher the Secrets
+            of the Ribosome.
           </p>
         </div>
       </div>
