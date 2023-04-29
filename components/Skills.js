d28ab61fef19aa1babbfa1439be78f8bfc59c8e4
jaswanth <jaswanthbeere@gmail.com>
2023-04-29 12:00:08 +0530

    small fix

--- components/Skills.js
+++ components/Skills.js
@@ -5,7 +5,7 @@ const Skills = () => {
     <div className="bg-white/70 h-fit md:h-[90vh] rounded-md shadow-2xl mx-auto lg:mx-[2rem] my-[2rem]  py-[2rem] px-[1rem] md:p-[2rem] w-[90%] lg:w-[60%] text-black  mb-[3rem]">
       <div className="">
         <h1 className="min-[300px]:text-[3.5rem] text-[4rem] md:text-[4.5rem] lg:text-[4.5rem]  xl:text-[5rem] 2xl:text-[5.5rem] my-2 lg:my-5 text-center md:text-left">
-          My Skills.
+          My Works.
         </h1>
         <div className="min-[300px]:text-[1rem] text-[1.1rem] md:text-[1.2rem] lg:text-[1.2rem] 2xl:text-[1.25rem] flex flex-col gap-4 leading-7 lg:leading-8 xl:leading-10">
           <p>
